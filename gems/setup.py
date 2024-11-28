3618c3db4f6c4f8dbbb548010c26b0532b7c45ce
abhishekcms611@gmail.com <abhishekcms611@gmail.com>
2024-11-28 05:03:12 +0000

    Add initial project setup with SQL macros for column parsing, renaming, and expressions

---
+++ gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'AlteryxDatabricks',
+    version = '0.0.1',
+    packages = ['AlteryxDatabricks'],
+    package_dir = {'AlteryxDatabricks': '.'},
+    description = '',
+    install_requires = [],
+)
+
